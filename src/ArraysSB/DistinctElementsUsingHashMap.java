8c1d8ed5e04696a5acaf9f29ac77b6710903c7a6
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-12-22 19:42:43 +0530

    ok

---
+++ src/ArraysSB/DistinctElementsUsingHashMap.java
@@ -0,0 +1,35 @@
+package ArraysSB;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DistinctElementsUsingHashMap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Distinct elements in the array:");
+        printDistinctElements(array);
+    }
+
+    private static void printDistinctElements(int[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : array) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+           // if (entry.getValue() == 1) {
+                System.out.print(entry.getKey() + " ");
+            //}
+        }
+    }
+}
